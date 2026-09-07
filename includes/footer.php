d3fb37cece7d91e50a7ee4d6c21f3804617862b4
Idkmymans <pranaya.manandhar.2007@gmail.com>
2026-09-07 18:18:28 +0545

    aaaaa

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,15 @@
+<head>
+    <!-- Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- Our own custom styles (must come AFTER bootstrap) -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+
 <footer>
     <div class="container">
         <div class="row">
@@ -25,3 +37,5 @@
         </div>
     </div>
 </footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,15 @@
+<head>
+    <!-- Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- Our own custom styles (must come AFTER bootstrap) -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
 <nav class="navbar navbar-expand-lg bg-white py-3">
     <div class="container">
         <a class="navbar-brand brand-name" href="#">Rate My Teacher</a>
@@ -17,3 +29,7 @@
         </div>
     </div>
 </nav>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+
+</body>
